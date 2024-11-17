7c9c392fd391a8372eb33f56924abb71ae42d9af
cotrin8672 <gummy8672@gmail.com>
2024-11-18 03:41:10 +0900

    fix: MOD情報の修正

--- src/main/java/io/github/cotrin8672/KotlinTemplateMod.kt
+++ src/main/java/io/github/cotrin8672/KotlinTemplateMod.kt
@@ -5,6 +5,6 @@ import net.neoforged.fml.common.Mod
 @Mod(KotlinTemplateMod.MOD_ID)
 class KotlinTemplateMod {
     companion object {
-        const val MOD_ID = "kotlintemplatemod"
+        const val MOD_ID = "mimictoolbox"
     }
 }
